692c468fbbb2959aa2e063a362fe8ffef7170117
Geethapriya Elavarasasn <93533793+Geethapriya03@users.noreply.github.com>
2024-06-18 11:18:57 +0530

    Create App.js

---
+++ Registration and authentication/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Registration from './Registration';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>API Request and Response</h1>
+      </header>
+      <main>
+        <Registration />
+      </main>
+    </div>
+  );
+}
+
+export default App;
